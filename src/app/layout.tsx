37771d226657bc47f0b2baa72a9064f1587d07b5
mianzegregory <mianzegregory@outlook.com>
2025-02-22 19:04:30 +0300

    commrnt out auth btns for now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,13 +36,13 @@ export default function RootLayout({
      <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <header className="flex justify-end items-center p-4 gap-4 h-16">
-          <SignedOut>
+          {/* <SignedOut>
             <SignInButton />
             <SignUpButton />
           </SignedOut>
           <SignedIn>
             <UserButton />
-          </SignedIn>
+          </SignedIn> */}
         </header>
         {children}
       </body>
