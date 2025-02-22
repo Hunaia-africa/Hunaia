003f5b05a1eabcc0145932f2c48e172f916e85e8
mianzegregory <mianzegregory@outlook.com>
2025-02-22 19:04:09 +0300

    conditionally render pages in entry file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,26 @@
-"use client";
+import { JSX } from "react";
+import ComingSoonPage from "./coming_soon/page";
+import Maintenance from "./maintenance/page";
 
-import styled, { keyframes } from "styled-components";
-import Image from "next/image";
-import Logo from "@/assets/logotype.svg";
+const Home: ({}) => Promise<JSX.Element> = async ({}) => {
+  
+  //variables to set maintenance mode or coming soon page
+  // Ensure one is always true and other is false
+  // When both are false, ensure home page is rendered
+  const maintenance: boolean = false;
+  const comingSoon: boolean = true;
 
-const spin = keyframes`
-  0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg); }
-`;
-
-const Page = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  padding: 1rem;
-  gap: 32px;
-  font-family: var(--font-geist-sans);
-  background-color: #bae0da;
-`;
-
-const Spinner = styled.div`
-  border: 0.5rem solid rgba(255, 255, 255, 0.3);
-  border-top: 0.5rem solid #34413e;
-  border-radius: 50%;
-  width: 3rem;
-  height: 3rem;
-  animation: ${spin} 1.5s linear infinite;
-`;
-
-const StyledImage = styled(Image)`
-  max-width: 100%;
-  height: auto;
-`;
-
-
-export default function Home() {
   return (
-    <Page>
-      <StyledImage alt="Logo" src={Logo} />
-      <p style={{ color: "#34413e", fontSize: "1.5rem", fontWeight: "500" }}>
-        Coming soon
-      </p>
-      <Spinner />
-    </Page>
+    <>
+      {comingSoon && !maintenance ? (
+        <ComingSoonPage />
+      ) : maintenance && !comingSoon ? (
+        <Maintenance />
+      ) : (
+        <main></main>
+      )}
+    </>
   );
-}
+};
+
+export default Home;
