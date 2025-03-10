15a8edd6df1ead8703d89afdac66834fa2ff282c
mianzegregory <mianzegregory@outlook.com>
2025-03-10 19:58:54 +0300

    add sections' skeletons

---
+++ src/app/(unauthed)/home/(sections)/exploreKenya.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const ExploreKenyaSection: FC = () => {
+  return <section></section>;
+};
+
+export default ExploreKenyaSection;
---
+++ src/app/(unauthed)/home/(sections)/gallery.tsx
@@ -0,0 +1,8 @@
+import { Section } from "@/components/layout";
+import { FC } from "react";
+
+const GallerySection: FC = () => {
+  return <Section id="gallery-section"></Section>;
+};
+
+export default GallerySection;
